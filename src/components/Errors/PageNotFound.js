8a17890de5fde0db34441c5901ed9096bc447609
ttkapostol <ttkapostolaki@gmail.com>
2023-03-23 07:00:28 +0100

    Add scss to page not found & check correct route

--- src/components/Errors/PageNotFound.js
+++ src/components/Errors/PageNotFound.js
@@ -1,17 +1,34 @@
+import { Link } from "react-router-dom";
+import PageNotFoundImg from "../../Images/pageNotFound.png";
+import "../../styles/layout/pageNotFound.scss";
+
 const PageNotFound = () => {
   return (
-    <article className="pageNotFound">
-      <h1>Ups! The page you are looking for does not exist.</h1>
-      <p>
-        Or maybe we don't have the hero you are looking ofr in our collection,
-        yet!
-      </p>
-      <img
-        alt="Page or Hero not Found"
-        title="Page or Hero not Found"
-        src="https://i.postimg.cc/GmVDFMsM/no-picture-2.png"
-      />
-    </article>
+    <>
+      <section className="container">
+        <article className="pageNotFound">
+          <h2 className="pageNotFound__title">
+            Ups! The page you are looking for does not exist.
+          </h2>
+          <p className="pageNotFound__text">
+            Or maybe we don't have the hero you are looking for in our
+            collection, yet!
+          </p>
+          <img
+            alt="Graphic of the golden snitch"
+            title="Graphic of the golden snitch"
+            src={PageNotFoundImg}
+          />
+        </article>
+        <Link to="/">
+          <input
+            type="button"
+            value="Go back to the list of characters"
+            className="btn404"
+          />
+        </Link>
+      </section>
+    </>
   );
 };
 
